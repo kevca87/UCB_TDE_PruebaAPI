a4f5e5b54061d9cac8977b2dbf620aa36ae98988
Kevin <kevcespedesarancibia87@gmail.com>
2022-08-16 00:21:57 -0400

    Cors enabled

--- Iter0_Backend/Program.cs
+++ Iter0_Backend/Program.cs
@@ -9,15 +9,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddTransient<IKidService, KidService>();
 builder.Services.AddSingleton<IAppRepository, AppRepository>();
-
+builder.Services.AddCors(c =>
+{
+    c.AddPolicy("AllowOrigin", options => { options.AllowAnyOrigin(); options.AllowAnyMethod(); options.AllowAnyHeader(); });
+});
 //AutoMapper
-
 builder.Services.AddAutoMapper(typeof(KidService));
 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(options => { options.AllowAnyOrigin(); options.AllowAnyMethod(); options.AllowAnyHeader(); });
 
 app.UseAuthorization();
 
